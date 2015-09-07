bb397c38e6fa5039030868e845131856de6f6db7
Tony Aldridge <tony@dabapps.com>
2015-09-07 13:29:52 +0100

    Added Underscore

---
+++ app/database.tcl
@@ -0,0 +1,39 @@
+namespace eval ::chores::database {
+    proc chores_for_day {week day} {
+        return [list \
+            [dict create \
+                title "Wash dishes" \
+                description "Self explanatory" \
+            ] \
+            [dict create \
+                title "Mop floor" \
+                description "Self explanatory" \
+            ] \
+            [dict create \
+                title "Cuddle kitty" \
+                description "Pretty important stuff" \
+            ] \
+        ]
+    }
+
+    proc chores_for_week {week} {
+        return [list \
+            [chores_for_day 0 $week] \
+            [chores_for_day 1 $week] \
+            [chores_for_day 2 $week] \
+            [chores_for_day 3 $week] \
+            [chores_for_day 4 $week] \
+            [chores_for_day 5 $week] \
+            [chores_for_day 6 $week] \
+        ]
+    }
+
+    proc all_chores {} {
+        return [dict create \
+            week_a [chores_for_week a]\
+            week_b [chores_for_week b]\
+            week_c [chores_for_week c]\
+            week_d [chores_for_week d]\
+        ]
+    }
+}
