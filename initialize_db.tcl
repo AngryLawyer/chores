3741831b584a0fa92649a02c11d65d8aae1fcad7
Tony Aldridge <tony@dabapps.com>
2015-10-15 13:04:27 +0100

    Corrected table creation

--- initialize_db.tcl
+++ initialize_db.tcl
@@ -4,14 +4,14 @@ package require sqlite3
 sqlite3 db $path
 
 db eval {CREATE TABLE chores (
-    id INT PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT NOT NULL,
     description TEXT NOT NULL
 );}
 
 db eval {CREATE TABLE chore_for_day (
-    id INT PRIMARY KEY AUTOINCREMENT,
-    chore_id INT NOT NULL,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    chore_id INTEGER NOT NULL,
     week CHAR(1) NOT NULL,
     DAY INT NOT NULL
 );}
